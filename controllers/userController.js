fbc014e81d4b18fabc0cd3df29ff1b839d12e346
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-06 21:27:42 +0530

    function added. updateUser stil completed but not work

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,6 +19,7 @@ function writeUsers(users) {
     writeFileSync(filePath, JSON.stringify(users, null, 2));
 }
 
+
 const getAllUsers = (req, res) => {
   const users = readUsers();
   res.json(users);
@@ -26,49 +27,62 @@ const getAllUsers = (req, res) => {
 
 
  const getUserById = (req, res) => {
-  const id = Number(req.params.userID);
-  const users = users.find(user => user.id === id);
+   const users = readUsers();
+  const id = parseInt(req.params.id);
+  const user = users.find(user => user.id === id);
   if (!user) {
     return res.status(404).send('User not found');
   }
   res.json(user);
 };
 
+
 const createUser = (req, res) => {
+  const users = readUsers();
+
+  const email = req.body.email;
+ const userEmail = users.find(user => user.email === email);
+ if (userEmail) {
+    return res.status(404).send('email is already registered');
+  }
   const newUser = {
     id: users.length + 1,
     name: req.body.name,
     email: req.body.email,
   };
+
   users.push(newUser);
-  res.status(201).json(newUser);
+
+  writeUsers(users);
+  res.status(201).send("user added");
 };
 
+
 const updateUser = (req, res) => {
-  const id = Number(req.params.userID);
+  const id = parseInt(req.params.id);
   const index = users.findIndex(user => user.id === id);
   if (index === -1) {
     return res.status(404).send('User not found');
   }
   const updatedUser = {
-    id: users[index].id,
-    name: req.body.name,
-    email: req.body.email,
+    id: req.params.id
   };
   users[index] = updatedUser;
   res.status(200).json('User updated');
 };
 
+
  const deleteUser = (req, res) => {
-  const id = Number(req.params.userID);
+  const users = readUsers();
+  const id = parseInt(req.params.id);
   const index = users.findIndex(user => user.id === id);
-  if (index === -1) {
+  if (!index === -1) {
     return res.status(404).send('User not found');
   }
   users.splice(index, 1);
+  writeUsers(users);
   res.status(200).json('User deleted');
 };
-
 
 export { getAllUsers,
          getUserById,
